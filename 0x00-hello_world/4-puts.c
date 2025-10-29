534673f77d4dc247f5672d7dad8eb2d4c37bb8df
ayindeme <ayindej1@.com>
2025-10-29 16:22:10 +0100

    program that prints exactly this line, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Author: Ayinde Temitope Jamiu
+ * Program; Winmingle Community C Training
+ * Description: Program that prints out output using puts
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return  (0);
+}  
+ 
+
+
+
+
