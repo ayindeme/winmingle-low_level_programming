d279151fde4c65e1d0e6def32fda7deff12c5b7e
ayindeme <ayindej1@.com>
2025-10-29 16:26:45 +0100

    using formatted output printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Author: Ayinde Temitope Jamiu
+ * Program; Winmingle Community C Training
+ * Description: program that print to the screen using formatted output printf
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return  (0);
+}
