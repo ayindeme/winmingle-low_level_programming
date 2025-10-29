c3cbd55c9615bf178b758b9e3fda10fabe2732a9
ayindeme <ayindej1@.com>
2025-10-29 16:40:14 +0100

    program that print using write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * Author: ayinde Temitope Jamiu
+ * Program: Winmingle Community C Training
+ * Description: Printing without printf and puts
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return  (1);
+}
